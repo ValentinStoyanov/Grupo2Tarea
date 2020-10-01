30d474e7f469a2e6aa75275197fd17cdf343b230
Ismael Rabanal Martín <Ismael.Rabanal@alu.uclm.es>
2020-10-01 19:51:53 +0200

    Initial structure

--- Grupo2Tarea/src/main/java/com/example/Grupo2/Grupo2TareaApplication.java
+++ Grupo2Tarea/src/main/java/com/example/Grupo2/Grupo2TareaApplication.java
@@ -3,15 +3,11 @@ package com.example.Grupo2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import capa.conexion.mongodb.AgentDB;
-
 @SpringBootApplication
 public class Grupo2TareaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Grupo2TareaApplication.class, args);
-		
-		
 	}
 
 }
