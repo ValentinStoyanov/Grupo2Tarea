b027334383b54008a4c34fed1f46e82a0ae19e37
Ismael Rabanal Martín <Ismael.Rabanal@alu.uclm.es>
2020-10-03 19:58:55 +0200

    Añadido contenido a las clases de modelo y repository para Tarea y listaTarea. Añadida la conexion a mongodb en el archivo application.properties

--- Grupo2Tarea/src/main/java/com/persistence/ListaTareasRepository.java
+++ Grupo2Tarea/src/main/java/com/persistence/ListaTareasRepository.java
@@ -1,5 +1,10 @@
 package com.persistence;
 
-public class ListaTareasRepository {
+import org.springframework.stereotype.Repository;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import com.model.ListaTareas;
+
+@Repository
+public interface ListaTareasRepository extends MongoRepository<ListaTareas, String> {
 
 }
--- Grupo2Tarea/src/main/java/com/persistence/TareaRepository.java
+++ Grupo2Tarea/src/main/java/com/persistence/TareaRepository.java
@@ -1,5 +1,10 @@
 package com.persistence;
 
-public class TareaRepository {
+import org.springframework.stereotype.Repository;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import com.model.Tarea;
+
+@Repository
+public interface TareaRepository extends MongoRepository<Tarea, String> {
 
 }
