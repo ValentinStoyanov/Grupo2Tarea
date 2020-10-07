2e7e281c2b3cb877ef0f8736b9b6923cddda499e
SaraSevillano <Sara.Sevillano1@alu.uclm.es>
2020-10-07 18:42:32 +0200

    prueba

---
+++ Grupo2Tarea/src/test/java/pasos/PruebaDeCasos.java
@@ -0,0 +1,14 @@
+package pasos;
+
+import cucumber.api.junit.Cucumber;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.And;
+import cucumber.api.java.en.When;
+import cucumber.api.java.en.Then;
+
+public class PruebaDeCasos{
+	
+	
+	
+	
+}
